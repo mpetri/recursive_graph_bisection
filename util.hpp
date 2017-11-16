afca0fb8ce0c5603233b5a4da3f88d198cd4849a
Matthias Petri <Matthias.Petri@gmail.com>
2017-11-16 22:53:38 +1100

    fix to small list skipping

--- util.hpp
+++ util.hpp
@@ -117,7 +117,7 @@ inverted_index read_d2si_docs(std::string docs_file, int min_list_len)
             const auto& list = read_uint32_list(df);
             size_t n = list.size();
             if (n < min_list_len)
-                break;
+                continue;
             max_doc_id = std::max(max_doc_id, list.back());
             num_lists++;
             num_postings += n;
