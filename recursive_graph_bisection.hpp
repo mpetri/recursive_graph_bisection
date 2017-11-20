eb07ea8a5ad2a578e7744bb337223e0189ad326f
Matthias Petri <Matthias.Petri@gmail.com>
2017-11-20 20:57:28 +1100

    remove spam msgs

--- recursive_graph_bisection.hpp
+++ recursive_graph_bisection.hpp
@@ -279,7 +279,6 @@ void compute_gains_nonparallel(docid_node* docs, size_t n,
 
 move_gains_t compute_move_gains(partition_t& P, size_t num_queries)
 {
-    timer t("compute_move_gains");
     move_gains_t gains;
 
     // (1) compute current partition cost deg1/deg2
@@ -287,7 +286,6 @@ move_gains_t compute_move_gains(partition_t& P, size_t num_queries)
     std::vector<float> left2right(num_queries);
     std::vector<float> right2left(num_queries);
     {
-        timer t("compute before after");
         std::vector<float> deg1(num_queries, 0);
         std::vector<float> deg2(num_queries, 0);
         {
@@ -321,7 +319,6 @@ move_gains_t compute_move_gains(partition_t& P, size_t num_queries)
     }
 
     // (2) compute gains from moving docs
-    timer t2("compute gains");
     cilk_spawn compute_gains(P.V1, P.n1, before, left2right, gains.V1);
     cilk_spawn compute_gains(P.V2, P.n2, before, right2left, gains.V2);
     cilk_sync;
@@ -331,7 +328,6 @@ move_gains_t compute_move_gains(partition_t& P, size_t num_queries)
 
 move_gains_t compute_move_gains_nonparallel(partition_t& P, size_t num_queries)
 {
-    timer t("compute_move_gains");
     move_gains_t gains;
 
     // (1) compute current partition cost deg1/deg2
@@ -339,7 +335,6 @@ move_gains_t compute_move_gains_nonparallel(partition_t& P, size_t num_queries)
     std::vector<float> left2right(num_queries);
     std::vector<float> right2left(num_queries);
     {
-        timer t("compute before after");
         std::vector<float> deg1(num_queries, 0);
         std::vector<float> deg2(num_queries, 0);
         {
