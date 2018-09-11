f5bf410b380cdd2694e3380c42cbfc2ee0f8e0f3
Antonio Mallia <me@antoniomallia.it>
2018-09-11 17:37:39 -0400

    Added test with actual float

--- test/test_rgb.cpp
+++ test/test_rgb.cpp
@@ -7,8 +7,8 @@ TEST(compute_single_gain, single_term)
     uint32_t terms[] = { 3 };
     docid_node doc{ 0, terms, 1, 1 };
     auto doc_ref = std::ref(doc);
-    std::vector<float> before = { 0, 1, 2, 3, 4, 5 };
-    std::vector<float> after = { 1, 1, 1, 2, 3, 4 };
+    std::vector<float> before = { 0.5, 1, 2, 3, 4, 5 };
+    std::vector<float> after = { 1.5, 1, 1, 2, 3, 4 };
     auto move_gain = compute_single_gain(&doc_ref, before, after);
 
     ASSERT_FLOAT_EQ(1, move_gain.gain);
@@ -19,8 +19,8 @@ TEST(compute_single_gain, multi_term)
     uint32_t terms[] = { 1, 2, 3, 4, 5};
     docid_node doc{ 0, terms, 5, 5 };
     auto doc_ref = std::ref(doc);
-    std::vector<float> before = { 0, 1, 2, 3, 4, 5 };
-    std::vector<float> after = { 1, 1, 1, 2, 3, 4 };
+    std::vector<float> before = { 0.5, 1, 2, 3, 4, 5 };
+    std::vector<float> after = { 1.5, 1, 1, 2, 3, 4 };
     auto move_gain = compute_single_gain(&doc_ref, before, after);
 
     ASSERT_FLOAT_EQ(4, move_gain.gain);
@@ -31,8 +31,8 @@ TEST(compute_single_gain, multi_term_negative)
     uint32_t terms[] = { 0, 1, 2, 3, 4, 5};
     docid_node doc{ 0, terms, 6, 6 };
     auto doc_ref = std::ref(doc);
-    std::vector<float> before = { 0, 1, 2, 3, 4, 5 };
-    std::vector<float> after = { 1, 1, 1, 2, 3, 4 };
+    std::vector<float> before = { 0.5, 1, 2, 3, 4, 5 };
+    std::vector<float> after = { 1.5, 1, 1, 2, 3, 4 };
     auto move_gain = compute_single_gain(&doc_ref, before, after);
 
     ASSERT_FLOAT_EQ(3, move_gain.gain);
